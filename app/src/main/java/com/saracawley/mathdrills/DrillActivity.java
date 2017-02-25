4fcac0d5994179060e9ac9a74dfde4d8ca428e47
scaw76 <scawley@dmail.dixie.edu>
2017-02-24 18:33:51 -0700

    first commit

---
+++ app/src/main/java/com/saracawley/mathdrills/DrillActivity.java
@@ -0,0 +1,13 @@
+package com.saracawley.mathdrills;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drill);
+    }
+}
